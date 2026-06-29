e7232e205d19f2f41d8c0b49178d3c7cb914d762
VarshiniParthasarathi <varshini.inbox05@gmail.com>
2026-06-29 19:03:09 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        a=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    a.append([i,j])
+                    break
+        return a[0]
+        
