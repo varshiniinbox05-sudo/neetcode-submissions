fda59d269cca88373b564ea71735302b58788517
VarshiniParthasarathi <varshini.inbox05@gmail.com>
2026-09-10 19:32:26 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=0
+        for n in nums:
+            result=n^result
+            
+        return result
+        
